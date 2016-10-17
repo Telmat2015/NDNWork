f9c1f24d873e613622b9a15768fe4f9cf58a2d6d
Syaiful Ahdan <syaifulahdan@gmail.com>
2016-10-17 18:40:55 +0700

    Add files via upload

---
+++ SecenarioNDN-Inherent-5p5c/drop-graph.R
@@ -0,0 +1,31 @@
+#!/usr/bin/env Rscript
+# Copyright (c) 2012-2015  Alexander Afanasyev <alexander.afanasyev@ucla.edu>
+
+
+# install.packages ('ggplot2')
+library(ggplot2)
+## # install.packages ('scales')
+## library (scales)
+
+#########################
+# Rate trace processing #
+#########################
+data = read.table("/home/bertopeng17/ndnSIM/ns-3/drop-trace.txt", header=T)
+data$Node = factor(data$Node)
+data$Kilobits <- data$Kilobytes * 8
+data$Type = factor(data$Type)
+
+## data.rtr = data[grep("Rtr", data$Node),]
+
+# graph rates on all nodes in Kilobits
+g.all <- ggplot(data, aes(x=Time, y=Kilobits, color=Type)) +
+  geom_point(size=2) +
+  geom_line() +
+  ylab("Packet drop rate [Kbits/s]") +
+  facet_wrap(~ Node) +
+  theme_bw()
+
+png("/home/bertopeng17/Desktop/l2-rate-tracer.png", width=800, height=500)
+print(g.all)
+x = dev.off()
+
